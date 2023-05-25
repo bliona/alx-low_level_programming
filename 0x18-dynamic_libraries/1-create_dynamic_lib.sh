020ca33494b809de8a70fb76dbe1785b3073c3c9
bliona <lionamuhati16@gmail.com>
2023-05-25 15:32:17 -0700

    Python

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
+gcc -Wall  -Wextra -Werror  -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
